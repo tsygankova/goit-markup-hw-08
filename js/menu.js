96ba88f8195fa83b74ecbaedb43fe56363332f92
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-08-27 10:02:00 +0300

    header mobile-first

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-  ​
-    menuBtnRef.addEventListener("click", () => {
+  ​  menuBtnRef.addEventListener("click", () => {
       const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
   ​
